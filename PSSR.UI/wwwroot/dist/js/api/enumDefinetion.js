ad852cba1a7c6c1392f4459f346c3aa10f4563cd
i.am.a.m.i.n <sahranavard.amin@gmail.com>
2019-09-29 00:00:13 +0330

    last9292019aminsahra

---
+++ PSSR.UI/wwwroot/dist/js/api/enumDefinetion.js
@@ -0,0 +1,22 @@
+﻿var enumDefinetion = enumDefinetion || (function () {
+    return {
+        getProjectType: function (eId) {
+            return getProjectType(eId);
+        }
+    };
+
+    function getProjectType(eId)
+    {
+        switch (eId)
+        {
+            case 1001:
+                return "Oil Platform";
+            case 1002:
+                return "Gas Platform";
+            case 1003:
+                return "Refinery";
+            case 1004:
+                return "Other";
+        }
+    }
+}());
